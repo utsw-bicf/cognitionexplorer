b820b2b3d7f0015aae303a39db47231f62312c43
Sensecode <mznie@hotmail.com>
2020-06-04 22:34:07 -0500

    add dependencies and pattern

---
+++ src/encoded/types/master_id.py
@@ -0,0 +1,31 @@
+from snovault import (
+    calculated_property,
+    collection,
+    load_schema,
+)
+from .base import (
+    Item,
+    # SharedItem,
+    paths_filtered_by_status,
+)
+import re
+
+
+@collection(
+    name="master_id",
+    unique_key="uuid",
+    properties={
+        "title": "Redcap_master_id Forms",
+        "description": "Redcap master_id forms: Basic patient information pages",
+    },
+)
+class Master_id(Item):
+    item_type = "master_id"
+    schema = load_schema("encoded:schemas/master_id.json")
+    embedded = [
+    ]
+    rev = {
+    }
+    audit_inherit = []
+    set_status_up = []
+    set_status_down = []
