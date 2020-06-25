feedf840526f5ea56c9d0fba68e068721f03ef0e
Sensecode <mznie@hotmail.com>
2020-06-25 17:01:39 -0500

    link master_id with ivp_a1

--- src/encoded/types/master_id.py
+++ src/encoded/types/master_id.py
@@ -13,7 +13,7 @@ import re
 
 @collection(
     name="master_id",
-    unique_key="ptid",
+    unique_key="uuid",
     properties={
         "title": "Redcap_master_id Forms",
         "description": "Redcap master_id forms: Basic patient information pages",
@@ -22,11 +22,25 @@ import re
 class Master_id(Item):
     item_type = "master_id"
     schema = load_schema("encoded:schemas/master_id.json")
-    name_key = 'ptid'
+    name_key ='uuid'
     embedded = [
+        'ivp_a1'
     ]
     rev = {
+        'ivp_a1': ('Ivp_a1', 'master_id'),
     }
+
     audit_inherit = []
     set_status_up = []
     set_status_down = []
+
+    @calculated_property(schema={
+        "title": "Ivp_a1",
+        "type": "array",
+        "items": {
+            "type": 'string',
+            "linkTo": "Ivp_a1"
+        },
+    })
+    def ivp_a1(self, request, ivp_a1):
+        return paths_filtered_by_status(request, ivp_a1)
