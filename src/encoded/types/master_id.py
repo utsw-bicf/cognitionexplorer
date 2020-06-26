8fc49d2709a3a32877ea92579da2b5c766d8c3ec
Sensecode <mznie@hotmail.com>
2020-06-26 15:22:03 -0500

    add ivp_a2 schema, insert data. embed header, link with master_id

--- src/encoded/types/master_id.py
+++ src/encoded/types/master_id.py
@@ -25,11 +25,15 @@ class Master_id(Item):
     name_key ='uuid'
     embedded = [
         'ivp_a1',
-        'fvp_a1'
+        'fvp_a1',
+        'ivp_a2',
+
     ]
     rev = {
         'ivp_a1': ('Ivp_a1', 'master_id'),
         'fvp_a1': ('Fvp_a1', 'master_id'),
+        'ivp_a2': ('Ivp_a2', 'master_id'),
+
 
     }
 
@@ -59,3 +63,14 @@ class Master_id(Item):
     })
     def fvp_a1(self, request, fvp_a1):
         return paths_filtered_by_status(request, fvp_a1)
+    
+    @calculated_property(schema={
+        "title": "Ivp_a2",
+        "type": "array",
+        "items": {
+            "type": 'string',
+            "linkTo": "Ivp_a2"
+        },
+    })
+    def ivp_a2(self, request, ivp_a2):
+        return paths_filtered_by_status(request, ivp_a2)
