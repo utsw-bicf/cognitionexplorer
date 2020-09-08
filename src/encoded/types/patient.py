2988cdcdb4a8236473d7abe306b538a721afb58f
mingjie <mingjiecn@gmail.com>
2020-09-08 18:40:27 -0500

    clean up and fix bug

--- src/encoded/types/patient.py
+++ src/encoded/types/patient.py
@@ -1,12 +1,3 @@
-from pyramid.view import (
-    view_config,
-)
-from pyramid.security import (
-    Allow,
-    Deny,
-    Everyone,
-)
-from pyramid.traversal import find_root
 from snovault import (
     calculated_property,
     collection,
@@ -14,463 +5,90 @@ from snovault import (
 )
 from .base import (
     Item,
-    SharedItem,
+    # SharedItem,
     paths_filtered_by_status,
 )
-from snovault.resource_views import item_view_object
-from snovault.util import expand_path
-from collections import defaultdict
-from datetime import datetime
-import math
+from pyramid.traversal import find_root, resource_path
 
-
-ONLY_ADMIN_VIEW_DETAILS = [
-    (Allow, 'group.admin', ['view', 'view_details', 'edit']),
-    (Allow, 'group.read-only-admin', ['view', 'view_details']),
-    (Allow, 'remoteuser.INDEXER', ['view']),
-    (Allow, 'remoteuser.EMBED', ['view']),
-    (Deny, Everyone, ['view', 'view_details', 'edit']),
-]
-
-USER_ALLOW_CURRENT = [
-    (Allow, Everyone, 'view'),
-] + ONLY_ADMIN_VIEW_DETAILS
-
-USER_DELETED = [
-    (Deny, Everyone, 'visible_for_edit')
-] + ONLY_ADMIN_VIEW_DETAILS
-
-
-def group_values_by_lab(request, labs):
-    values_by_key = defaultdict(list)
-    for path in labs:
-        properties = request.embed(path, '@@object?skip_calculated=true')
-        values_by_key[properties.get('lab')].append(properties)
-    return dict(values_by_key)
-
-
-def group_values_by_vital(request, vitals):
-    values_by_key = defaultdict(list)
-    for path in vitals:
-        properties = request.embed(path, '@@object?skip_calculated=true')
-        values_by_key[properties.get('vital')].append(properties)
-    return dict(values_by_key)
-
-
-def supportive_med_frequency(request, supportive_medication):
-    frequency_by_key = defaultdict(list)
-    supportive_meds = []
-    for path in supportive_medication:
-        properties = request.embed(path, '@@object?skip_calculated=true')
-        start_date = properties.get('start_date')
-        frequency_by_key[properties.get('name')].append(start_date)
-
-    for k, v in frequency_by_key.items():
-        med_freq = {}
-        med_freq['name'] = k
-        med_freq['start_date'] = min(v)
-        med_freq['frequency'] = len(v)
-        supportive_meds.append(med_freq)
-    return supportive_meds
-
+import re
 
 @collection(
-     name='patients',
-     unique_key='accession',
-     properties={
-         'title': 'Patients',
-         'description': 'Listing Patients',
-})
+    name="patients",
+    unique_key="accession",
+    properties={
+        "title": "Redcap_master_id Forms",
+        "description": "Redcap master_id forms: Basic patient information pages",
+    },
+)
 class Patient(Item):
-    item_type = 'patient'
-    schema = load_schema('encoded:schemas/patient.json')
-    name_key = 'accession'
+    item_type = "patient"
+    schema = load_schema("encoded:schemas/patient.json")
+    name_key ="accession"
     embedded = [
-        'labs',
-        'vitals',
-        'germline',
-        'consent',
-        'radiation',
-        'medical_imaging',
-        'medications',
-        'supportive_medications',
-        'surgery'
+        'ivp_a1',
+        'fvp_a1',
+        'ivp_a2',
+        'fvp_a2'
+
     ]
     rev = {
-        'labs': ('LabResult', 'patient'),
-        'vitals': ('VitalResult', 'patient'),
-        'germline': ('Germline', 'patient'),
-        'consent': ('Consent', 'patient'),
-        'radiation': ('Radiation', 'patient'),
-        'medical_imaging': ('MedicalImaging', 'patient'),
-        'medication': ('Medication', 'patient'),
-        'supportive_medication': ('SupportiveMedication', 'patient'),
-        'surgery': ('Surgery', 'patient')
+        'ivp_a1': ('Ivp_a1', 'patient'),
+        'fvp_a1': ('Fvp_a1', 'patient'),
+        'ivp_a2': ('Ivp_a2', 'patient'),
+        'fvp_a2': ('Fvp_a2', 'patient')
+
+
+
     }
+
+    audit_inherit = [
+        
+    ]
     set_status_up = []
     set_status_down = []
 
-    @calculated_property( schema={
-        "title": "Labs",
-        "type": "array",
-        "items": {
-            "type": "string",
-            "linkTo": "LabResult",
-        },
-    })
-    def labs(self, request, labs):
-        return group_values_by_lab(request, labs)
-
-    @calculated_property( schema={
-        "title": "Vitals",
-        "type": "array",
-        "items": {
-            "type": "string",
-            "linkTo": "VitalResult",
-        },
-    })
-    def vitals(self, request, vitals):
-        return group_values_by_vital(request, vitals)
-
     @calculated_property(schema={
-        "title": "Germline Mutations",
+        "title": "Ivp_a1",
         "type": "array",
         "items": {
             "type": 'string',
-            "linkTo": "Germline"
+            "linkTo": "Ivp_a1"
         },
     })
-    def germline(self, request, germline):
-        return paths_filtered_by_status(request, germline)
-
-    @calculated_property(condition='germline', schema={
-        "title": "Positive Germline Mutations",
-        "type": "array",
-        "items": {
-            "type": "string",
-        },
-    })
-    def germline_summary(self, request, germline):
-        germline_summary = []
-        for mutation in germline:
-            mutatation_object = request.embed(mutation, '@@object')
-            if mutatation_object['significance'] in ['Positive', 'Variant', 'Positive and Variant']:
-                mutation_summary = mutatation_object['target']
-                germline_summary.append(mutation_summary)
-        return germline_summary
-
-
+    def ivp_a1(self, request, ivp_a1):
+        return paths_filtered_by_status(request, ivp_a1)
+    
+    
     @calculated_property(schema={
-        "title": "Radiation Treatment",
+        "title": "Fvp_a1",
         "type": "array",
         "items": {
             "type": 'string',
-            "linkTo": "Radiation"
+            "linkTo": "Fvp_a1"
         },
     })
-    def radiation(self, request, radiation):
-        return paths_filtered_by_status(request, radiation)
-
-    @calculated_property(define=True, schema={
-        "title": "Radiation Treatment Summary",
-        "type": "string",
-    })
-    def radiation_summary(self, request, radiation=None):
-        if len(radiation) > 0:
-            radiation_summary = "Treatment Received"
-        else:
-            radiation_summary = "No Treatment Received"
-        return radiation_summary
-
-    @calculated_property(condition='radiation', schema={
-        "title": "Dose per Fraction",
-        "type": "array",
-        "items": {
-            "type": "string",
-        },
-    })
-    def dose_range(self, request, radiation):
-        dose_range = []
-        for treatment in radiation:
-            treatment_object = request.embed(treatment, '@@object')
-            if treatment_object['dose']/treatment_object['fractions'] < 2000:
-                dose_range.append("200 - 2000")
-            elif treatment_object['dose']/treatment_object['fractions'] < 4000:
-                dose_range.append("2000 - 4000")
-            else:
-                dose_range.append("4000 - 6000")
-        return dose_range
-
-    @calculated_property(condition='radiation', schema={
-        "title": "Radiation Fractions",
-        "type": "array",
-        "items": {
-            "type": "string",
-        },
-    })
-    def fractions_range(self, request, radiation):
-        fractions_range = []
-        for treatment in radiation:
-            treatment_object = request.embed(treatment, '@@object')
-            if treatment_object['fractions'] < 5:
-                fractions_range.append("1 - 5")
-            elif treatment_object['fractions'] < 10:
-                fractions_range.append("5 - 10")
-            elif treatment_object['fractions'] < 15:
-                fractions_range.append("10 - 15")
-            else:
-                fractions_range.append("15 and up")
-        return fractions_range
-
-
+    def fvp_a1(self, request, fvp_a1):
+        return paths_filtered_by_status(request, fvp_a1)
+    
     @calculated_property(schema={
-        "title": "Medical Imaging",
+        "title": "Ivp_a2",
         "type": "array",
         "items": {
             "type": 'string',
-            "linkTo": "MedicalImaging"
+            "linkTo": "Ivp_a2"
         },
     })
-    def medical_imaging(self, request, medical_imaging):
-        return paths_filtered_by_status(request, medical_imaging)
-
+    def ivp_a2(self, request, ivp_a2):
+        return paths_filtered_by_status(request, ivp_a2)
 
     @calculated_property(schema={
-        "title": "Consent",
+        "title": "Fvp_a2",
         "type": "array",
         "items": {
             "type": 'string',
-            "linkTo": "Consent"
+            "linkTo": "Fvp_a2"
         },
     })
-    def consent(self, request, consent):
-        return paths_filtered_by_status(request, consent)
-
-    @calculated_property( schema={
-        "title": "Medications",
-        "type": "array",
-        "items": {
-            "type": "string",
-            "linkTo": "Medication",
-        },
-    })
-    def medications(self, request, medication):
-        return paths_filtered_by_status(request, medication)
-
-    @calculated_property(condition='medication', schema={
-        "title": "Medication duration",
-        "type": "array",
-        "items": {
-            "type": "string",
-        },
-    })
-    def medication_range(self, request, medication):
-
-        for object in medication:
-            medication_object = request.embed(object, '@@object')
-            date_format="%Y-%m-%d"
-            start_date=datetime.strptime(medication_object['start_date'],date_format )
-            end_date=datetime.strptime(medication_object['end_date'],date_format )
-            medication_duration=(end_date-start_date).days/30
-
-            medication_range=[]
-            if 0<=medication_duration<3:
-                medication_range.append("0-3 months")
-            elif 3<=medication_duration<6:
-                medication_range.append("3-6 months")
-            elif 6<=medication_duration<9:
-                medication_range.append("6-9 months")
-            elif 9<=medication_duration<12:
-                medication_range.append("9-12 months")
-            elif 12<=medication_duration<18:
-                medication_range.append("12-18 months")
-            elif 18<=medication_duration<24:
-                medication_range.append("18-24 months")
-            elif 24<=medication_duration<30:
-                medication_range.append("24-30 months")
-            elif 30<=medication_duration<36:
-                medication_range.append("30-36 months")
-            elif 36<=medication_duration<48:
-                medication_range.append("36-48 months")
-            else :
-                medication_range.append("48+ months")
-        return medication_range
-
-    @calculated_property( schema={
-        "title": "Supportive Medications",
-        "type": "array",
-        "items": {
-            "type": "string",
-            "linkTo": "SupportiveMedication",
-        },
-        })
-
-    def supportive_medications(self, request, supportive_medication):
-        return supportive_med_frequency(request, supportive_medication)
-
-
-    @calculated_property( schema={
-        "title": "Surgeries",
-        "type": "array",
-        "items": {
-            "type": "string",
-            "linkTo": "Surgery",
-        },
-    })
-
-    def surgery(self, request, surgery):
-        return paths_filtered_by_status(request, surgery)
-
-    @calculated_property(define=True, schema={
-            "title": "Surgery Treatment Summary",
-            "type": "string",
-        })
-    def surgery_summary(self, request, surgery=None):
-            if len(surgery) > 0:
-                surgery_summary = "Treatment Received"
-            else:
-                    surgery_summary = "No Treatment Received"
-            return surgery_summary
-
-@collection(
-    name='lab-results',
-    properties={
-        'title': 'Lab results',
-        'description': 'Lab results pages',
-    })
-class LabResult(Item):
-    item_type = 'lab_results'
-    schema = load_schema('encoded:schemas/lab_results.json')
-    embeded = []
-
-
-@collection(
-    name='vital-results',
-    properties={
-        'title': 'Vital results',
-        'description': 'Vital results pages',
-    })
-class VitalResult(Item):
-    item_type = 'vital_results'
-    schema = load_schema('encoded:schemas/vital_results.json')
-    embeded = []
-
-
-@collection(
-    name='germline',
-    properties={
-        'title': 'Germline Mutations',
-        'description': 'Germline Mutation results pages',
-    })
-class Germline(Item):
-    item_type = 'germline'
-    schema = load_schema('encoded:schemas/germline.json')
-    embeded = []
-
-@collection(
-    name='consent',
-    properties={
-        'title': 'Consent',
-        'description': 'Consent results pages',
-    })
-class Consent(Item):
-    item_type = 'consent'
-    schema = load_schema('encoded:schemas/consent.json')
-    embeded = []
-
-
-@collection(
-    name='radiation',
-    properties={
-        'title': 'Radiation treatment',
-        'description': 'Radiation treatment results pages',
-    })
-class Radiation(Item):
-    item_type = 'radiation'
-    schema = load_schema('encoded:schemas/radiation.json')
-    embeded = []
-
-    @calculated_property(condition='dose', schema={
-        "title": "Dosage per Fraction",
-        "type": "number",
-    })
-    def dose_per_fraction(self, request, dose, fractions):
-        dose_per_fraction = dose/fractions
-        return dose_per_fraction
-
-
-@collection(
-    name='medical_imaging',
-    properties={
-        'title': 'Medical imaging',
-        'description': 'Medical imaging results pages',
-    })
-class MedicalImaging(Item):
-    item_type = 'medical-imaging'
-    schema = load_schema('encoded:schemas/medical_imaging.json')
-    embeded = []
-
-
-@collection(
-    name='medication',
-    properties={
-        'title': 'Medications',
-        'description': 'Medication results pages',
-    })
-class Medication(Item):
-    item_type = 'medication'
-    schema = load_schema('encoded:schemas/medication.json')
-    embeded = []
-
-
-@collection(
-    name='supportive-medication',
-    properties={
-        'title': 'Supportive Medications',
-        'description': 'Supportive Medication results pages',
-    })
-class SupportiveMedication(Item):
-    item_type = 'supportive_medication'
-    schema = load_schema('encoded:schemas/supportive_medication.json')
-    embeded = []
-
-
-@collection(
-    name='surgery',
-    properties={
-        'title': 'Surgeries',
-        'description': 'Surgeries results pages',
-    })
-class Surgery(Item):
-    item_type = 'surgery'
-    schema = load_schema('encoded:schemas/surgery.json')
-    embeded = []
-
-@property
-def __name__(self):
-    return self.name()
-
-
-@view_config(context=Patient, permission='view', request_method='GET', name='page')
-def patient_page_view(context, request):
-    if request.has_permission('view_details'):
-        properties = item_view_object(context, request)
-    else:
-        item_path = request.resource_path(context)
-        properties = request.embed(item_path, '@@object')
-    for path in context.embedded:
-        expand_path(request, properties, path)
-    return properties
-
-
-@view_config(context=Patient, permission='view', request_method='GET',
-             name='object')
-def patient_basic_view(context, request):
-    properties = item_view_object(context, request)
-    filtered = {}
-    for key in ['@id', '@type', 'accession', 'uuid', 'gender', 'ethnicity', 'race', 'age', 'age_units', 'status', 'labs', 'vitals', 'germline', 'germline_summary','radiation', 'radiation_summary', 'dose_range', 'fractions_range', 'medical_imaging', 'medications','medication_range', 'supportive_medications','surgery_summary']:
-        try:
-            filtered[key] = properties[key]
-        except KeyError:
-            pass
-    return filtered
+    def fvp_a2(self, request, fvp_a2):
+        return paths_filtered_by_status(request, fvp_a2)
+    
