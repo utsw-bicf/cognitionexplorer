bb0ba4537e795c005f3c51b0033e22a2702697da
Sensecode <mznie@hotmail.com>
2020-11-04 08:20:40 -0600

    connect with patient

--- src/encoded/types/patient.py
+++ src/encoded/types/patient.py
@@ -37,6 +37,7 @@ class Patient(Item):
         'ivp_b7',
         'ivp_b8',
         'ivp_b9',
+        'ivp_c2',
         'ivp_d2',
     ]
     rev = {
@@ -52,6 +53,7 @@ class Patient(Item):
         'ivp_b7': ('Ivp_b7', 'patient'),
         'ivp_b8': ('Ivp_b8', 'patient'),
         'ivp_b9': ('Ivp_b9', 'patient'),
+        'ivp_c2': ('Ivp_c2', 'patient'),
         'ivp_d2': ('Ivp_d2', 'patient'),
     }
 
@@ -193,6 +195,18 @@ class Patient(Item):
     })
     def ivp_b9(self, request, ivp_b9):
         return paths_filtered_by_status(request, ivp_b9)
+
+    @calculated_property(schema={
+        "title": "Ivp_c2",
+        "type": "array",
+        "items": {
+            "type": 'string',
+            "linkTo": "Ivp_c2"
+        },
+    })
+    def ivp_c2(self, request, ivp_c2):
+        return paths_filtered_by_status(request, ivp_c2)
+
       
     @calculated_property(schema={
         "title": "Ivp_d2",
