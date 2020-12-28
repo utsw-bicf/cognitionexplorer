0b00a5f38d87e91a3cbcfd099a71319397291f09
Sensecode <mznie@hotmail.com>
2020-12-28 12:07:10 -0600

    add tvp_b5, connect with patient, put on showpage

--- src/encoded/static/components/formsTable.js
+++ src/encoded/static/components/formsTable.js
@@ -184,6 +184,12 @@ class FormsTable extends React.Component {
             forms[index].a1_id = data.tvp_a1[i]['@id']
             forms[index].a1_name = "tvp_a1"
         }
+        for (let i = 0; i < data.tvp_b5.length; i++) {
+            let index = followUpVisitTimes.indexOf(data.tvp_b5[i]["visdate"]) + 1;
+            // console.log(index)
+            forms[index].b5_id = data.tvp_b5[i]['@id']
+            forms[index].b5_name = "tvp_b5"
+        }
 
         return forms
     }
@@ -198,6 +204,7 @@ class FormsTable extends React.Component {
         data.fvp_d2.forEach(element => followUpVisitTimes.push(element["visdate"]))
         data.fvp_b8.forEach(element => followUpVisitTimes.push(element["visdate"]))
         data.tvp_a1.forEach(element => followUpVisitTimes.push(element["visdate"]))
+        data.tvp_b5.forEach(element => followUpVisitTimes.push(element["visdate"]))
         followUpVisitTimes = [...new Set(followUpVisitTimes)];
         followUpVisitTimes = Array.from(followUpVisitTimes);
         followUpVisitTimes.sort(function (a, b) {
--- src/encoded/types/patient.py
+++ src/encoded/types/patient.py
@@ -45,6 +45,7 @@ class Patient(Item):
         'fvp_d2',
         'fvp_b8',
         'tvp_a1',
+        'tvp_b5',
     ]
     rev = {
         'ivp_a1': ('Ivp_a1', 'patient'),
@@ -67,6 +68,7 @@ class Patient(Item):
         'fvp_d2': ('Fvp_d2', 'patient'),
         'fvp_b8': ('Fvp_b8', 'patient'),
         'tvp_a1': ('Tvp_a1', 'patient'),
+        'tvp_b5': ('Tvp_b5', 'patient'),
     }
 
     audit_inherit = [
@@ -295,3 +297,14 @@ class Patient(Item):
     })
     def tvp_a1(self, request, tvp_a1):
         return paths_filtered_by_status(request, tvp_a1)
+
+    @calculated_property(schema={
+        "title": "Tvp_b5",
+        "type": "array",
+        "items": {
+            "type": 'string',
+            "linkTo": "Tvp_b5"
+        },
+    })
+    def tvp_b5(self, request, tvp_b5):
+        return paths_filtered_by_status(request, tvp_b5)
