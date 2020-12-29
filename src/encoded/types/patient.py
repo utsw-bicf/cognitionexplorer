0a9df30692d2757ea218ee516d1a87ae62328daf
Sensecode <mznie@hotmail.com>
2020-12-28 18:16:01 -0600

    add tvp_d2 form, connect with patient, put on show page

--- src/encoded/types/patient.py
+++ src/encoded/types/patient.py
@@ -45,6 +45,7 @@ class Patient(Item):
         'fvp_d2',
         'fvp_b8',
         'tvp_a1',
+        'tvp_d2',
     ]
     rev = {
         'ivp_a1': ('Ivp_a1', 'patient'),
@@ -67,6 +68,7 @@ class Patient(Item):
         'fvp_d2': ('Fvp_d2', 'patient'),
         'fvp_b8': ('Fvp_b8', 'patient'),
         'tvp_a1': ('Tvp_a1', 'patient'),
+        'tvp_d2': ('Tvp_d2', 'patient'),
     }
 
     audit_inherit = [
@@ -295,3 +297,14 @@ class Patient(Item):
     })
     def tvp_a1(self, request, tvp_a1):
         return paths_filtered_by_status(request, tvp_a1)
+
+    @calculated_property(schema={
+        "title": "Tvp_d2",
+        "type": "array",
+        "items": {
+            "type": 'string',
+            "linkTo": "Tvp_d2"
+        },
+    })
+    def tvp_d2(self, request, tvp_d2):
+        return paths_filtered_by_status(request, tvp_d2)
