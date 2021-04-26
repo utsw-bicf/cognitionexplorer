ec9656d876f1c03341881580e195617b477176e9
Mingzhu Nie <mznie@hotmail.com>
2021-04-26 11:43:37 -0500

    restructure ivp_b6 to ivp_b6v2 and ivp_b6v3

--- src/encoded/types/ivp_b6.py
+++ src/encoded/types/ivp_b6.py
@@ -1,32 +1,74 @@
 from snovault import (
+    abstract_collection,
     calculated_property,
     collection,
     load_schema,
 )
+# from snovault.util import Path
+from pyramid.security import Authenticated
 from .base import (
     Item,
-    # SharedItem,
     paths_filtered_by_status,
 )
 import re
 
 
-@collection(
+@abstract_collection(
     name="ivp_b6s",
     unique_key="uuid",
     properties={
         "title": "UDS_IVP_B6 Forms",
         "description": "UDS Initial visiting patients B6 forms results pages",
-    },
-)
+    })
 class Ivp_b6(Item):
-    item_type = "ivp_b6"
-    schema = load_schema("encoded:schemas/ivp_b6.json")
+    base_types = ['Ivp_b6'] + Item.base_types
     embedded = [
-       
+        
     ]
     rev = {
     }
+
+
+audit_inherit = []
+set_status_up = []
+set_status_down = []
+
+
+@collection(
+    name='ivp_b6v3',
+    unique_key='uuid',
+    properties={
+        "title": "UDS_IVP_B6V3 Forms",
+        "description": "UDS Initial visiting patients B6V3 forms results pages",
+    })
+class Ivp_b6v3(Ivp_b6):
+    item_type = 'ivp_b6v3'
+    schema = load_schema('encoded:schemas/ivp_b6v3.json')
+    embedded = Ivp_b6.embedded + []
+    rev = {
+    }
     audit_inherit = []
-    set_status_up = []
+    set_status_up = [
+
+    ]
+    set_status_down = []
+
+
+@collection(
+    name='ivp_b6v2',
+    unique_key='uuid',
+    properties={
+        "title": "UDS_IVP_B6V2 Forms",
+        "description": "UDS Initial visiting patients B6V2 forms results pages",
+    })
+class Ivp_b6v2(Ivp_b6):
+    item_type = 'ivp_b6v2'
+    schema = load_schema('encoded:schemas/ivp_b6v2.json')
+    embedded = Ivp_b6.embedded + []
+    rev = {
+    }
+    audit_inherit = []
+    set_status_up = [
+
+    ]
     set_status_down = []
