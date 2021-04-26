249951a0744e33fb1d7e1f4f2aaacac92830c4dc
Mingzhu Nie <mznie@hotmail.com>
2021-04-26 11:27:47 -0500

    restructure ivp_b5 to ivp_b5v2 and ivp_b5v3

---
+++ src/encoded/schemas/changelogs/ivp_b5v2.md
@@ -0,0 +1,6 @@
+## Changelog for ivp_b5v2.json
+
+
+### Schema version 1
+
+* New schema for uds ivp_b5v2 added
--- src/encoded/types/ivp_b5.py
+++ src/encoded/types/ivp_b5.py
@@ -1,32 +1,74 @@
 from snovault import (
+    abstract_collection,
     calculated_property,
     collection,
     load_schema,
 )
+# from snovault.util import Path
+from pyramid.security import Authenticated
 from .base import (
     Item,
-    # SharedItem,
     paths_filtered_by_status,
 )
 import re
 
 
-@collection(
+@abstract_collection(
     name="ivp_b5s",
     unique_key="uuid",
     properties={
         "title": "UDS_IVP_B5 Forms",
         "description": "UDS Initial visiting patients B5 forms results pages",
-    },
-)
+    })
 class Ivp_b5(Item):
-    item_type = "ivp_b5"
-    schema = load_schema("encoded:schemas/ivp_b5.json")
+    base_types = ['Ivp_b5'] + Item.base_types
     embedded = [
-       
+        
     ]
     rev = {
     }
+
+
+audit_inherit = []
+set_status_up = []
+set_status_down = []
+
+
+@collection(
+    name='ivp_b5v3',
+    unique_key='uuid',
+    properties={
+        "title": "UDS_IVP_B5V3 Forms",
+        "description": "UDS Initial visiting patients B5V3 forms results pages",
+    })
+class Ivp_b5v3(Ivp_b5):
+    item_type = 'ivp_b5v3'
+    schema = load_schema('encoded:schemas/ivp_b5v3.json')
+    embedded = Ivp_b5.embedded + []
+    rev = {
+    }
     audit_inherit = []
-    set_status_up = []
+    set_status_up = [
+
+    ]
+    set_status_down = []
+
+
+@collection(
+    name='ivp_b5v2',
+    unique_key='uuid',
+    properties={
+        "title": "UDS_IVP_B5V2 Forms",
+        "description": "UDS Initial visiting patients B5V2 forms results pages",
+    })
+class Ivp_b5v2(Ivp_b5):
+    item_type = 'ivp_b5v2'
+    schema = load_schema('encoded:schemas/ivp_b5v2.json')
+    embedded = Ivp_b5.embedded + []
+    rev = {
+    }
+    audit_inherit = []
+    set_status_up = [
+
+    ]
     set_status_down = []
