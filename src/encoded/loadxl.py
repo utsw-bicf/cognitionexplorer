239d111d59ba8ac07821402d4a611e6baa4806e8
Mingzhu Nie <mznie@hotmail.com>
2021-04-27 11:35:32 -0500

    restructure ivp_a5 to ivp_a5v2 and ivp_a5v3

--- src/encoded/loadxl.py
+++ src/encoded/loadxl.py
@@ -31,7 +31,8 @@ ORDER = [
 
     'ivp_a4',
     'ivp_a3',
-    'ivp_a5',
+    'ivp_a5v3',
+    'ivp_a5v2',
     'fvp_a2',
     'ivp_b5v2',
     'ivp_b5v3',
--- src/encoded/types/ivp_a5.py
+++ src/encoded/types/ivp_a5.py
@@ -1,32 +1,74 @@
 from snovault import (
+    abstract_collection,
     calculated_property,
     collection,
     load_schema,
 )
+# from snovault.util import Path
+from pyramid.security import Authenticated
 from .base import (
     Item,
-    # SharedItem,
     paths_filtered_by_status,
 )
 import re
 
 
-@collection(
+@abstract_collection(
     name="ivp_a5s",
     unique_key="uuid",
     properties={
         "title": "UDS_IVP_A5 Forms",
         "description": "UDS Initial visiting patients A5 forms results pages",
-    },
-)
+    })
 class Ivp_a5(Item):
-    item_type = "ivp_a5"
-    schema = load_schema("encoded:schemas/ivp_a5.json")
+    base_types = ['Ivp_a5'] + Item.base_types
     embedded = [
-       
+        
     ]
     rev = {
     }
+
+
+audit_inherit = []
+set_status_up = []
+set_status_down = []
+
+
+@collection(
+    name='ivp_a5v3',
+    unique_key='uuid',
+    properties={
+        "title": "UDS_IVP_A5V3 Forms",
+        "description": "UDS Initial visiting patients A5V3 forms results pages",
+    })
+class Ivp_a5v3(Ivp_a5):
+    item_type = 'ivp_a5v3'
+    schema = load_schema('encoded:schemas/ivp_a5v3.json')
+    embedded = Ivp_a5.embedded + []
+    rev = {
+    }
     audit_inherit = []
-    set_status_up = []
+    set_status_up = [
+
+    ]
+    set_status_down = []
+
+
+@collection(
+    name='ivp_a5v2',
+    unique_key='uuid',
+    properties={
+        "title": "UDS_IVP_A5V2 Forms",
+        "description": "UDS Initial visiting patients A5V2 forms results pages",
+    })
+class Ivp_a5v2(Ivp_a5):
+    item_type = 'ivp_a5v2'
+    schema = load_schema('encoded:schemas/ivp_a5v2.json')
+    embedded = Ivp_a5.embedded + []
+    rev = {
+    }
+    audit_inherit = []
+    set_status_up = [
+
+    ]
     set_status_down = []
