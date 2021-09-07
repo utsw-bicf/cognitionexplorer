78da78f5fd543b48911bce009fb93ef500c33665
mingjie <mznie@hotmail.com>
2021-09-07 09:10:12 -0500

    add ivp_a1v3 version, edit patient files

---
+++ src/encoded/types/ivp_a1v3.py
@@ -0,0 +1,32 @@
+from snovault import (
+    calculated_property,
+    collection,
+    load_schema,
+)
+from .base import (
+    Item,
+    # SharedItem,
+    paths_filtered_by_status,
+)
+from pyramid.traversal import find_root, resource_path
+import re
+
+
+@collection(
+    name="ivp_a1v3s",
+    unique_key="uuid",
+    properties={
+        "title": "UDS_IVP_A1V3 Forms",
+        "description": "UDS initial visits forms A1V3:Subject Demographic results pages",
+    },
+)
+class Ivp_a1v3(Item):
+    item_type = "ivp_a1v3"
+    schema = load_schema("encoded:schemas/ivp_a1v3.json")
+    embedded = [
+    ]
+    rev = {
+    }
+    audit_inherit = []
+    set_status_up = []
+    set_status_down = []
